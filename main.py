30a7b3779cc7a65f7e5a16b7fa573e082a87b8ed
wanjirumw-ui <wanjirumw123@gmail.com>
2026-07-20 11:34:47 +0300

    url shortener

---
+++ main.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import pyshorteners
+
+
+root = Tk()
+root.title('Link Shortener')
+# root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("500x500")
+
+def shorten():
+    if shorty.get():
+        shorty.delete(0, END)
+
+
+    if my_entry.get():
+        url = pyshorteners.Shortener().tinyurl.short(my_entry.get())
+
+        shorty.insert(END, url)
+
+my_label = Label(root,text="Enter Link to shorten",font=("Helvetica",34))
+my_label.pack(pady=20)
+
+my_entry = Entry(root,font=("Helvetica",24))
+my_entry.pack(pady=20)
+
+my_button = Button(root, text="Shorten Link", command=shorten, font=("Helvetica",24))
+my_button.pack(pady=20)
+
+shorty_label = Label(root,text="Shortened Link", font=("Helvetica", 14))
+shorty_label.pack(pady=50)
+
+shorty = Entry(root, font=("Helvetica", 22),justify=CENTER)
+shorty.pack(pady=10)
+
+
+root.mainloop()
